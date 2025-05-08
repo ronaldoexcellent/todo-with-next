fc09eb9adf8e64c507710ccd8c6bd04f0bd9ee63
Ronaldo Excellent <120473458+ronaldoexcellent@users.noreply.github.com>
2025-05-08 15:32:10 +0100

    Delete src directory

--- src/components/svg/Bubbles.tsx
+++
@@ -1,8 +0,0 @@
-export default function Bubbles() {
-    return (
-        <svg width="140" height="123" viewBox="0 0 200 183" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cy="83" r="100" fill="#8FE1D7" fillOpacity="0.5"/>
-            <circle cx="100" cy="13" r="100" fill="#8FE1D7" fillOpacity="0.5"/>
-        </svg>
-    )
-}
